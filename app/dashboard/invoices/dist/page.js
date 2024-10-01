5ec7425c50ca001f489cab252bea6981499ac391
Aboubacar Traore <aboubatrao04@gmail.com>
2024-10-01 13:14:05 +0100

    solving

--- app/dashboard/invoices/dist/page.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var react_1 = require("react");
-function Page() {
-    return react_1["default"].createElement("p", null, "Invoices Page");
-}
-exports["default"] = Page;
