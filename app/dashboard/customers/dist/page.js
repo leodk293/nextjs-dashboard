9f56070b19c3475062d4fcd1c64cae93367a521b
Aboubacar Traore <aboubatrao04@gmail.com>
2024-10-01 13:12:28 +0100

    solving

--- app/dashboard/customers/dist/page.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var react_1 = require("react");
-function Page() {
-    return react_1["default"].createElement("p", null, "Customers Page");
-}
-exports["default"] = Page;
