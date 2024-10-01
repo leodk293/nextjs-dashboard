381132b07086e5abf5aa012f5e5073f2e9f1d58e
Aboubacar Traore <aboubatrao04@gmail.com>
2024-10-01 13:02:23 +0100

    error handling

---
+++ app/dashboard/customers/dist/page.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+function Page() {
+    return react_1["default"].createElement("p", null, "Customers Page");
+}
+exports["default"] = Page;
---
+++ app/dashboard/invoices/dist/page.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+function Page() {
+    return react_1["default"].createElement("p", null, "Invoices Page");
+}
+exports["default"] = Page;
